2036c7a48781b25bb313019e8f39511e8ff40c04
Ostefar <56957832+Ostefar@users.noreply.github.com>
2021-05-10 10:22:15 +0200

    Initial commit

---
+++ app/src/main/java/com/ew/snapapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ew.snapapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
