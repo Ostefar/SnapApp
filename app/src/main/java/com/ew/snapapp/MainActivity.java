0db09e69a7c74f80030810fdc6fbf7db8aa10192
Ostefar <56957832+Ostefar@users.noreply.github.com>
2021-05-12 16:50:44 +0200

    getting ready for firebase

--- app/src/main/java/com/ew/snapapp/MainActivity.java
+++ app/src/main/java/com/ew/snapapp/MainActivity.java
@@ -28,8 +28,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
@@ -44,13 +47,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
+
+    private Uri mImageUri;
 
     Button btnLoadImage, btnSaveImage;
 
     private ImageView imageResult;
-    private ArrayList<String> pathArray;
     private EditText imageName;
-    private int array_position;
 
     private ProgressDialog mProgressDialog;
 
@@ -73,13 +78,10 @@ public class MainActivity extends AppCompatActivity {
         btnSaveImage = (Button)findViewById(R.id.mySaveBtn);
         imageResult = (ImageView)findViewById(R.id.myImageView);
         imageName = (EditText) findViewById(R.id.imageName);
-        pathArray = new ArrayList<>();
         mProgressDialog = new ProgressDialog(MainActivity.this);
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
-
-        //checkFilePermissions();
-        addFilePaths();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
 
         paintDraw = new Paint();
         paintDraw.setStyle(Paint.Style.FILL);
@@ -95,36 +97,10 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(intent, RQS_IMAGE1);
         });
 
-        /*btnSaveImage.setOnClickListener(v -> {
+        btnSaveImage.setOnClickListener(v -> {
             if(bitmapMaster != null){
                 saveBitmap(bitmapMaster);
             }
-        });*/
-
-        btnSaveImage.setOnClickListener(view -> {
-            Log.d(TAG, "onClick: Uploading Image.");
-            mProgressDialog.setMessage("Uploading Image...");
-            mProgressDialog.show();
-
-            String name = imageName.getText().toString();
-            if(!name.equals("")){
-                Uri uri = Uri.fromFile(new File(pathArray.get(array_position)));
-                StorageReference storageReference = mStorageRef.child("images/" + name + ".jpg");
-                storageReference.putFile(uri).addOnSuccessListener(taskSnapshot -> {
-                    // Get a URL to the uploaded content
-                    //Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                    toastMessage("Upload Success");
-                    mProgressDialog.dismiss();
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        toastMessage("Upload Failed");
-                        mProgressDialog.dismiss();
-                    }
-                })
-                ;
-            }
-
         });
 
 
@@ -251,40 +227,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void addFilePaths(){
-        Log.d(TAG, "addFilePaths: Adding file paths.");
-        String path = System.getenv("EXTERNAL_STORAGE");
-        pathArray.add(path+"/Pictures/Portal/image1.jpg");
-        pathArray.add(path+"/Pictures/Portal/image2.jpg");
-        pathArray.add(path+"/Pictures/Portal/image3.jpg");
-        loadImageFromStorage();
-    }
-
-    private void loadImageFromStorage()
-    {
-        try{
-            String path = pathArray.get(array_position);
-            File f=new File(path, "");
-            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
-            imageResult.setImageBitmap(b);
-        }catch (FileNotFoundException e){
-            Log.e(TAG, "loadImageFromStorage: FileNotFoundException: " + e.getMessage() );
-        }
-
-    }
-
-
-   /* private void checkFilePermissions() {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
-            int permissionCheck = MainActivity.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
-            permissionCheck += MainActivity.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
-            if (permissionCheck != 0) {
-                this.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1001); //Any number
-            }
-        }else{
-            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
-        }
-    }
 */
     /**
      * customizable toast
