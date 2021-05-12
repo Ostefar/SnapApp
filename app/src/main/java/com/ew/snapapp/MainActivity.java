f757a3b00ecd8e7a18fd598daae7a551965802c8
Ostefar <56957832+Ostefar@users.noreply.github.com>
2021-05-12 15:28:44 +0200

    load og tegn done

--- app/src/main/java/com/ew/snapapp/MainActivity.java
+++ app/src/main/java/com/ew/snapapp/MainActivity.java
@@ -1,25 +1,58 @@
 package com.ew.snapapp;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Context;
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private StorageReference mStorageRef;
+
     Button btnLoadImage, btnSaveImage;
+
+    private ImageView imageResult;
+    private ArrayList<String> pathArray;
+    private EditText imageName;
+    private int array_position;
+
+    private ProgressDialog mProgressDialog;
 
     final int RQS_IMAGE1 = 1;
 
@@ -28,98 +61,238 @@ public class MainActivity extends AppCompatActivity {
     Canvas canvasMaster;
 
     int prvX, prvY;
-    Paint paintDraw;
 
-    private ImageView imageView;
+    Paint paintDraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.myImageView);
+        btnLoadImage = (Button)findViewById(R.id.myLoadBtn);
+        btnSaveImage = (Button)findViewById(R.id.mySaveBtn);
+        imageResult = (ImageView)findViewById(R.id.myImageView);
+        imageName = (EditText) findViewById(R.id.imageName);
+        pathArray = new ArrayList<>();
+        mProgressDialog = new ProgressDialog(MainActivity.this);
 
-        Button chooseBtn = findViewById(R.id.myLoadBtn);
-        chooseBtn.setOnClickListener(view -> selectImage(MainActivity.this));
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        
-        //MIDLERTIDIG
-        Button sendBtn = findViewById(R.id.mySendBtn);
-        sendBtn.setOnClickListener(v -> goToChat());
+        //checkFilePermissions();
+        addFilePaths();
+
+        paintDraw = new Paint();
+        paintDraw.setStyle(Paint.Style.FILL);
+        paintDraw.setColor(Color.WHITE);
+        paintDraw.setStrokeWidth(10);
+
+        Button chatBtn = findViewById(R.id.myChatBtn);
+        chatBtn.setOnClickListener(v -> goToChat());
+
+        btnLoadImage.setOnClickListener(arg0 -> {
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, RQS_IMAGE1);
+        });
+
+        /*btnSaveImage.setOnClickListener(v -> {
+            if(bitmapMaster != null){
+                saveBitmap(bitmapMaster);
+            }
+        });*/
+
+        btnSaveImage.setOnClickListener(view -> {
+            Log.d(TAG, "onClick: Uploading Image.");
+            mProgressDialog.setMessage("Uploading Image...");
+            mProgressDialog.show();
+
+            String name = imageName.getText().toString();
+            if(!name.equals("")){
+                Uri uri = Uri.fromFile(new File(pathArray.get(array_position)));
+                StorageReference storageReference = mStorageRef.child("images/" + name + ".jpg");
+                storageReference.putFile(uri).addOnSuccessListener(taskSnapshot -> {
+                    // Get a URL to the uploaded content
+                    //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    toastMessage("Upload Success");
+                    mProgressDialog.dismiss();
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        toastMessage("Upload Failed");
+                        mProgressDialog.dismiss();
+                    }
+                })
+                ;
+            }
+
+        });
 
 
 
+        imageResult.setOnTouchListener((v, event) -> {
 
+            int action = event.getAction();
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    prvX = x;
+                    prvY = y;
+                    drawOnProjectedBitMap((ImageView) v, bitmapMaster, prvX, prvY, x, y);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    drawOnProjectedBitMap((ImageView) v, bitmapMaster, prvX, prvY, x, y);
+                    prvX = x;
+                    prvY = y;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    drawOnProjectedBitMap((ImageView) v, bitmapMaster, prvX, prvY, x, y);
+                    break;
+            }
+            /*
+             * Return 'true' to indicate that the event have been consumed.
+             * If auto-generated 'false', your code can detect ACTION_DOWN only,
+             * cannot detect ACTION_MOVE and ACTION_UP.
+             */
+            return true;
+        });
     }
+
+    /*
+    Project position on ImageView to position on Bitmap draw on it
+     */
+
+    private void drawOnProjectedBitMap(ImageView iv, Bitmap bm,
+                                       float x0, float y0, float x, float y){
+        if(x<0 || y<0 || x > iv.getWidth() || y > iv.getHeight()){
+            //outside ImageView
+            return;
+        }else{
+
+            float ratioWidth = (float)bm.getWidth()/(float)iv.getWidth();
+            float ratioHeight = (float)bm.getHeight()/(float)iv.getHeight();
+
+            canvasMaster.drawLine(
+                    x0 * ratioWidth,
+                    y0 * ratioHeight,
+                    x * ratioWidth,
+                    y * ratioHeight,
+                    paintDraw);
+            imageResult.invalidate();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_CANCELED) {
-            switch (requestCode) {
-                case 0:
-                    if (resultCode == RESULT_OK && data != null) {
-                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
-                        imageView.setImageBitmap(selectedImage);
-                    }
-                    break;
-                case 1:
-                    if (resultCode == RESULT_OK && data != null) {
-                        Uri selectedImage = data.getData();
-                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
-                        if (selectedImage != null) {
-                            Cursor cursor = getContentResolver().query(selectedImage,
-                                    filePathColumn, null, null, null);
-                            if (cursor != null) {
-                                cursor.moveToFirst();
 
-                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                                String picturePath = cursor.getString(columnIndex);
-                                imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-                                cursor.close();
-                            }
+        Bitmap tempBitmap;
+
+        if(resultCode == RESULT_OK){
+            switch (requestCode){
+                case RQS_IMAGE1:
+                    source = data.getData();
+
+                    try {
+                        //tempBitmap is Immutable bitmap,
+                        //cannot be passed to Canvas constructor
+                        tempBitmap = BitmapFactory.decodeStream(
+                                getContentResolver().openInputStream(source));
+
+                        Bitmap.Config config;
+                        if(tempBitmap.getConfig() != null){
+                            config = tempBitmap.getConfig();
+                        }else{
+                            config = Bitmap.Config.ARGB_8888;
                         }
+
+                        //bitmapMaster is Mutable bitmap
+                        bitmapMaster = Bitmap.createBitmap(
+                                tempBitmap.getWidth(),
+                                tempBitmap.getHeight(),
+                                config);
+
+                        canvasMaster = new Canvas(bitmapMaster);
+                        canvasMaster.drawBitmap(tempBitmap, 0, 0, null);
+
+                        imageResult.setImageBitmap(bitmapMaster);
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
                     }
+
                     break;
             }
         }
     }
-    private void selectImage(Context context) {
-        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle("Choose your profile picture");
+    private void saveBitmap(Bitmap bm){
+        File file = Environment.getExternalStorageDirectory();
+        File newFile = new File(file, "test.jpg");
 
-        builder.setItems(options, (dialog, item) -> {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(newFile);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            Toast.makeText(MainActivity.this,
+                    "Save Bitmap: " + fileOutputStream.toString(),
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,
+                    "Something wrong: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,
+                    "Something wrong: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 
-            if (options[item].equals("Take Photo")) {
-                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePicture, 0);
+    private void addFilePaths(){
+        Log.d(TAG, "addFilePaths: Adding file paths.");
+        String path = System.getenv("EXTERNAL_STORAGE");
+        pathArray.add(path+"/Pictures/Portal/image1.jpg");
+        pathArray.add(path+"/Pictures/Portal/image2.jpg");
+        pathArray.add(path+"/Pictures/Portal/image3.jpg");
+        loadImageFromStorage();
+    }
+
+    private void loadImageFromStorage()
+    {
+        try{
+            String path = pathArray.get(array_position);
+            File f=new File(path, "");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            imageResult.setImageBitmap(b);
+        }catch (FileNotFoundException e){
+            Log.e(TAG, "loadImageFromStorage: FileNotFoundException: " + e.getMessage() );
+        }
+
+    }
 
 
-            } else if (options[item].equals("Choose from Gallery")) {
-                Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(pickPhoto , 1);
-
-            } else if (options[item].equals("Cancel")) {
-                dialog.dismiss();
+   /* private void checkFilePermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = MainActivity.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
+            permissionCheck += MainActivity.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+            if (permissionCheck != 0) {
+                this.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1001); //Any number
             }
-        });
-        builder.show();
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
     }
-    // save edited photo to firebase
-    public void saveToDb(){
-
+*/
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
-    // Drawing text on image:
-
-    public Bitmap loadBitmapFromView(View v) {
-        Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(b);
-        v.layout(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
-        v.draw(c);
-        return b;
-    }
-
 
     public void goToChat(){
         Intent intent = new Intent(this, ChatActivity.class);
